062718ddc03854587584cad81b0c1e7bcf5c74f7
KO Myung-Hun <komh@chollian.net>
2024-06-22 23:35:19 +0900

    my: make use of 'high-mem' by default
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -6,6 +6,7 @@ n=configure
 test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
 
 export PKG_CONFIG="pkg-config --static"
+export LDFLAGS=-Zhigh-mem
 
 opts=""
 "$d/$n" $opts "$@" 2>&1 | tee "$n.log"
